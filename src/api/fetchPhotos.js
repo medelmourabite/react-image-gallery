b4cb4ff4d3ae13916248bf79eb736c13198f366c
medelmourabite <med.elmourabite@gmail.com>
2022-01-23 18:03:07 +0100

    cleanup

--- src/api/fetchPhotos.js
+++ src/api/fetchPhotos.js
@@ -1,28 +1,30 @@
-import axios from "axios";
-const clientId = "0d54d7bf8f81c9ee80a75d9e1263fbb6b8267fad9d908e597b9f7c4f6bcdee23";
+import axios from 'axios';
+const clientId =
+  '0d54d7bf8f81c9ee80a75d9e1263fbb6b8267fad9d908e597b9f7c4f6bcdee23';
 
-export const fetchPhotos = async (query = "", page = 1, filter = null, sort = null) => {
-    const apiUrl = `https://api.unsplash.com/${query ? "search/" : ""}photos`;
+export const fetchPhotos = async (searchQuery = '', page = 1) => {
+  const query = searchQuery.trim();
+  const apiUrl = `https://api.unsplash.com/${query ? 'search/' : ''}photos`;
 
-    try {
-        let params = {
-            client_id: clientId,
-            content_filter: "high",
-            per_page: 32,
-            page,
-            query,
-        };
+  try {
+    let params = {
+      client_id: clientId,
+      content_filter: 'high',
+      per_page: 32,
+      page,
+      query,
+    };
 
-        const result = await axios.get(apiUrl, { params });
-        if (result.status === 200) {
-            if (!query) {
-                return result.data;
-            } else {
-                return result.data.results;
-            }
-        }
-    } catch (error) {
-        console.error(error);
+    const result = await axios.get(apiUrl, { params });
+    if (result.status === 200) {
+      if (!query) {
+        return result.data;
+      } else {
+        return result.data.results;
+      }
     }
-    return [];
+  } catch (error) {
+    console.error(error);
+  }
+  return [];
 };
