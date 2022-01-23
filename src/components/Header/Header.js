0f4e33e1818103989fa224a12e9c8417649c7266
medelmourabite <med.elmourabite@gmail.com>
2022-01-23 15:33:04 +0100

    css fixes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,6 +4,9 @@ import {Pagination, SearchBar} from "../Common";
 
 const Header = ({setQuery, page, setPage}) => {
   return <div className="Header">
+    <div>
+      <h3>Image Gallery</h3>
+    </div>
     <SearchBar setQuery={setQuery}/>
     <Pagination page={page} setPage={setPage}/>
   </div>;
